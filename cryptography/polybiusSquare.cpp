e4605b52130dbc6e7ab31a51d6c578113453285a
Aleksey Anikeev <bebebe1317@gmail.com>
2019-09-19 23:21:41 +0300

    the "Tritemio" and "Polybius Square" ciphers was added

---
+++ cryptography/polybiusSquare.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <locale.h>
+#include <wchar.h>
+#include <string.h>
+#include <codecvt>
+#include <locale>
+
+
+class PolybyusSquare {
+public:
+    PolybyusSquare() {
+        int i = 1;
+        int j = 1;
+        for (auto begin = tableLetters.begin(), end = tableLetters.end(); begin != end; begin++) {
+            keysTable.insert({begin->first, j*10 + i});
+            if (i == SIZE_TABLE) {
+                j++;
+                i = 1;
+            }
+            i++;
+        } 
+    }
+    const std::vector<int> encrypt(const std::wstring& textToEncrypt);
+    const std::wstring decrypt(const std::vector<int>& ecnryptedText);
+    const wchar_t getKeyByValue(const int value);
+
+private:
+    const std::map<const wchar_t, const int> tableLetters = {
+        {L'\u0410', 472}, {L'\u0411', 473}, {L'\u0412',  474}, {L'\u0413', 475}, {L'\u0414', 476}, {L'\u0415', 477}, {L'\u0416', 478},
+        {L'\u0417', 479}, {L'\u0418', 480},  {L'\u0419', 481}, {L'\u041A', 482}, {L'\u041B', 483}, {L'\u041C', 484}, {L'\u041D', 485},
+        {L'\u041E', 486}, {L'\u041F', 487}, {L'\u0420', 488}, {L'\u0421', 489}, {L'\u0422', 490}, {L'\u0423', 491},
+        {L'\u0424', 492}, {L'\u0425', 493}, {L'\u0426', 494}, {L'\u0427', 495},	{L'\u0428', 496}, {L'\u0429', 497}, {L'\u042A', 498},
+        {L'\u042B', 499}, {L'\u042C', 500}, {L'\u042D', 501}, {L'\u042E', 502}, {L'\u042F', 503}
+    };
+    std::map<const wchar_t, const int> keysTable;
+    const int SIZE_TABLE = 6;
+};
+
+const std::vector<int> PolybyusSquare::encrypt(const std::wstring& decryptedText) {
+    std::vector<int> ecryptedText;
+    for (wchar_t letter: decryptedText)
+        ecryptedText.push_back(keysTable.find(letter)->second);
+    return ecryptedText;
+}
+
+const wchar_t PolybyusSquare::getKeyByValue(const int value) {
+    for (auto it : keysTable)
+        if (it.second == value)
+            return it.first;
+    return {};
+}
+
+const std::wstring PolybyusSquare::decrypt(const std::vector<int>& ecnryptedText) {
+    std::wstring decryptedText{};
+    for (size_t value: ecnryptedText)
+        decryptedText.push_back(getKeyByValue(value));
+
+    return decryptedText;
+}
+
+std::string ws2s(const std::wstring& wstr) {
+    using convert_typeX = std::codecvt_utf8<wchar_t>;
+    std::wstring_convert<convert_typeX, wchar_t> converterX;
+
+    return converterX.to_bytes(wstr);
+}
+
+
+int main() {
+    std::wstring decryptedText = L"АБВГД";
+    PolybyusSquare ps;
+    std::vector<int> encryptedText = ps.encrypt(decryptedText);
+    for (auto value: encryptedText)
+        std::cout << value << "\n";
+
+    std::cout << "decrypted: " << ws2s(ps.decrypt(encryptedText)) << "\n";
+    return 0;
+}
---
+++ cryptography/tritemio.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <iterator>
+#include <tuple>
+
+
+class Tritemio {
+public: 
+    Tritemio() = default;
+    Tritemio(const int alphabetOffset, const int widthOffset) {
+        int row = 1;
+        int column = 1;
+        for (size_t index = 0, size = tableLetters.size(); index < size; index++) {
+            if ((index + alphabetOffset) < size)
+                keysTable.insert({std::get<0>(tableLetters.at(index + alphabetOffset)), row * 10 + column});    
+            else
+                keysTable.insert({std::get<0>(tableLetters.at(size - (index + alphabetOffset))), row * 10 + column});
+
+            if (column == widthOffset) {
+                row++;
+                column = 1;
+            } else
+                column++;
+        }
+    }
+
+    const std::vector<const int> encrypt(const std::wstring& decryptedText);
+    const std::wstring decrypt(const std::vector<const int>& encrtyptedText);
+    const wchar_t getKeyByValue(const int value);
+
+private:
+    const std::vector<std::tuple<const wchar_t, const int>> tableLetters = {
+        {L'\u0410', 472}, {L'\u0411', 473}, {L'\u0412',  474}, {L'\u0413', 475}, {L'\u0414', 476}, {L'\u0415', 477}, {L'\u0416', 478},
+        {L'\u0417', 479}, {L'\u0418', 480},  {L'\u0419', 481}, {L'\u041A', 482}, {L'\u041B', 483}, {L'\u041C', 484}, {L'\u041D', 485},
+        {L'\u041E', 486}, {L'\u041F', 487}, {L'\u0420', 488}, {L'\u0421', 489}, {L'\u0422', 490}, {L'\u0423', 491},
+        {L'\u0424', 492}, {L'\u0425', 493}, {L'\u0426', 494}, {L'\u0427', 495},	{L'\u0428', 496}, {L'\u0429', 497}, {L'\u042A', 498},
+        {L'\u042B', 499}, {L'\u042C', 500}, {L'\u042D', 501}, {L'\u042E', 502}, {L'\u042F', 503}
+    };
+    const int numbersEnglish = 22;
+    std::map<const wchar_t, const int> keysTable;
+
+};
+
+const std::vector<const int> Tritemio::encrypt(const std::wstring& decryptedText) {
+    std::vector<int> encryptedText{};
+    auto end = keysTable.end();
+    for (auto key : decryptedText) {
+        auto valueIter = keysTable.find(key);
+        if (valueIter != end)
+            encryptedText.push_back(valueIter->second);
+        else
+            return {};
+    }
+
+}
+
+const wchar_t Tritemio::getKeyByValue(const int value) {
+    for (auto it : keysTable)
+        if (it.second == value)
+            return it.first;
+    return {};
+}
+
+const std::wstring Tritemio::decrypt(const std::vector<const int>& encrtyptedText) {
+    std::wstring dectypredText{}; 
+    for (auto value : encrtyptedText) {
+        wchar_t key = getKeyByValue(value);
+        if (key != NULL)
+            dectypredText.push_back(key);
+        else
+            return {};
+    }
+
+    return dectypredText;
+}
